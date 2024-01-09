b3686284d5a8b0b3734366f974201ccf283b7378
tomzy-ba <trbee100@gmail.com>
2024-01-09 18:51:50 +0000

    done 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(normalString) {
+    const stringToLetters = normalString.split('').reverse().join('');
+    return stringToLetters
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// hello
+// olleh
+
+// 
