936aee6012999d5836d39436988a7566f8bf32e3
tomzy-ba <trbee100@gmail.com>
2024-01-09 17:57:32 +0000

    done 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let answer = "";
+    for(num < 0; num --;) {
+        answer += string;
+    }
+    return answer;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// using return ends a function
