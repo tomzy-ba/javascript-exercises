bfd72ffa8b3dd05b02db88ffb1594b14773c7beb
tomzy-ba <trbee100@gmail.com>
2024-01-10 20:38:19 +0000

    done 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
+const add = function(numAdd1, numAdd2) {
+  return numAdd1 + numAdd2
+};
+
+const subtract = function(numSub1, numSub2) {
+  return numSub1 - numSub2
+};
+
+const sum = function(arraySum) {
+  let godSum = 0;
+  for (let i = 0; i < arraySum.length; i++) {
+    godSum += arraySum[i];
+  }
+  return godSum;
+}
+
+const multiply = function(arrayMulti) {
+  let godMulti = 1;
+  for (let i = 0; i < arrayMulti.length; i++) {
+    godMulti *= arrayMulti[i];
+  }
+  return godMulti;
+};
+
+const power = function(powerNum1, powerNum2) {
+  return powerNum1 ** powerNum2
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(factorNum) {
+  if (factorNum == 0 || factorNum == 1) {
+    return 1;
+  }
+  else {
+  let factorEndSum = 1;
+  for (let i = factorNum; i > 0; i--) {
+    factorEndSum *= i;
+  }
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  return factorEndSum
+  }
 };
 
 // Do not edit below this line
