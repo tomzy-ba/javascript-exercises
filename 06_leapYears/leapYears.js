69884b97c0cb37520a66bc991f4d77ac9694fa97
tomzy-ba <trbee100@gmail.com>
2024-01-10 17:31:46 +0000

    done 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,28 @@
-const leapYears = function() {
+/*
+    if checkYear / 4 = PASS
+    if checkYear / 100 = FALSE
+    if checkYear / 400 = PASS
+    AHA! use an else function to make it work
+    if checkYear / 400 pass
+    else if checkYear / 100 FALSE
+*/
 
-};
+
+
+const leapYears = function(checkYear) {
+    if (checkYear % 400 ==0) {
+        return true
+
+    }
+    else if (checkYear % 100 ==0) {
+        return false
+    }
+    else {
+    return(checkYear % 4 ==0) 
+    }
+    
+}
+
 
 // Do not edit below this line
 module.exports = leapYears;
